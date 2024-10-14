1c85046d1f26a7de159087d2eedb406e112f484e
KellyChen <matijueming@gmail.com>
2024-10-14 22:17:24 +0800

    Initial Commit

---
+++ Assignment1/Assignment1App.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment1App.swift
+//  Assignment1
+//
+//  Created by Jia Chen on 2024/10/14.
+//
+
+import SwiftUI
+
+@main
+struct Assignment1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
